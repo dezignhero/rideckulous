7892175de214ad3fd6cd0ff3dc7c162c0c630697
Alex Lea <jkdalex@gmail.com>
2013-04-24 18:14:30 -0700

    More changes

--- rideckulous.js
+++ rideckulous.js
@@ -6,7 +6,7 @@ var Deck = function(selector, options) {
 		animating = false,
 		numSlides = 0,
 		goTo = 0,
-		currentSlide = 0,
+		currentCard = 0,
 		lastSlide = 0;
 
 	// Swiping
@@ -18,8 +18,7 @@ var Deck = function(selector, options) {
 
 	// Defaults
 	var defaults = {
-		easeDefault : 0.2,
-		preventAdvance : false
+		easeDefault : 0.2
 	};
 
 	/*------- Initialization -------*/
@@ -42,9 +41,7 @@ var Deck = function(selector, options) {
 		numSlides = cards.length;
 		cards.each(function(i){
 			var self = $(this);
-			self.attr('data-id', i).css({
-				'z-index' : numSlides-i
-			});
+			self.attr('data-id', i).css({ 'z-index' : numSlides-i });
 			// Add initial class
 			if ( i == 0 ) self.addClass('current');
 		});
@@ -53,11 +50,7 @@ var Deck = function(selector, options) {
 		resize();
 
 		// Display controls correctly
-		if(defaults.preventAdvance) {
-			disableSliding();
-		} else {
-			updateControls();
-		}
+		updateControls();
 
 		// Behavior
 		controls.on('touchstart, click', function(){
@@ -65,10 +58,10 @@ var Deck = function(selector, options) {
 				animating = true;
 
 				var self = $(this);
-				if(self.hasClass('next') && (currentSlide < numSlides-1)) {
-					goTo = currentSlide + 1;
-				} else if (self.hasClass('prev') && currentSlide > 0) {
-					goTo = currentSlide - 1;
+				if(self.hasClass('next') && (currentCard < numSlides-1)) {
+					goTo = currentCard + 1;
+				} else if (self.hasClass('prev') && currentCard > 0) {
+					goTo = currentCard - 1;
 				}
 
 				// Move container
@@ -91,7 +84,7 @@ var Deck = function(selector, options) {
 
 		window.addEventListener(orientationEvent, function() {
 			resize(function(){
-				jumpToSlide(currentSlide);
+				jumpToSlide(currentCard);
 			});
 		}, false);
 	},
@@ -124,45 +117,49 @@ var Deck = function(selector, options) {
 
 			// Figure out closest slide
 			if(moved > 0 && moved > viewportWidth/3) {
-				if(currentSlide > 0) {
-					goTo = currentSlide - 1;
+				if(currentCard > 0) {
+					goTo = currentCard - 1;
 				}
 			} else if(moved < 0 && moved < -viewportWidth/3) {
-				if(currentSlide < numSlides-1) {
-					goTo = currentSlide + 1;
+				if(currentCard < numSlides-1) {
+					goTo = currentCard + 1;
 				}
 			} else {
-				goTo = currentSlide;
+				goTo = currentCard;
 			}
 
 			// Jump to closest
-			console.log(goTo);
+			console.log('goTo: '+goTo);
 			jumpToSlide(goTo, 0.15);
 		}
 	},
 	
 	touchMove = function(e) {
-		if(swipe.started && !parent.hasClass('disabled')) {
-			var touchX = e.touches ? e.touches[0].pageX : e.pageX;
-			var touchY = e.touches ? e.touches[0].pageY : e.pageY;
-			var dX = touchX - swipe.startX;
-			var dY = touchY - swipe.startY;
+		if ( swipe.started ) {
+			var touchX = e.touches ? e.touches[0].pageX : e.pageX,
+				touchY = e.touches ? e.touches[0].pageY : e.pageY,
+				dX = touchX - swipe.startX,
+				dY = touchY - swipe.startY;
+			
 			swipe.endX = touchX;
 			
 			// Escape if motion wrong
-			if(Math.abs(dX) < Math.abs(dY)) return true;
+			if ( Math.abs(dX) < Math.abs(dY) ) return true;
 
 			e.preventDefault();
 
 			// Always run this so that hit the ends
-			animate(dX,'none');
+			var card = ( dX <= 0 ) ? $('.page.current') : $('.page.top');
+			if ( card.length > 0 ) {
+				animate(card, dX,'none');
+			}
 		}
 	},
 	
-	animate = function(scrollTo, ease) {
+	animate = function(card, scrollTo, ease) {
 		// Momentum Effect or Not
-		var transition = (ease!='none') ? 'all '+ease+'s ease-out' : 'none';
-		var card = ( scrollTo < 0 ) ? card = $('.page.current') : $('.page.top');
+		var transition = ( ease != 'none' ) ? 'all '+ease+'s ease-out' : 'none';
+
 		card[0].style.webkitTransition = transition;
 		card[0].style.webkitTransform = 'translate3d('+scrollTo+'px,0,0)';
 
@@ -170,6 +167,8 @@ var Deck = function(selector, options) {
 		window.setTimeout(function(){
 			animating = false;
 		}, ease*1000);
+
+		return card;
 	},
 
 	jumpToSlide = function(num, ease) {
@@ -179,56 +178,53 @@ var Deck = function(selector, options) {
 			// Animate
 			var easeAmt = ease || defaults.easeDefault;
 			animating = true;
-			animate(-viewportWidth, easeAmt);
 
-			// If new slide
-			if(num != currentSlide) {
-				// Update current slide
-				currentSlide = num;
-				cards.removeClass('current');
-				$(cards.selector+'[data-id='+currentSlide+']').addClass('current');
+			// Determine how to move slides
+			var cc = $(cards.selector+'[data-id='+currentCard+']');
 
-				// Control Buttons
-				updateControls();
+			if(num == currentCard) {
+				animate(cc, 0, easeAmt);
+			} else {
+				var nc = $(cards.selector+'[data-id='+num+']');
 
-				// Disable Again
-				if(defaults.preventAdvance) {
-					disableSliding();
+				// How to move slides in
+				if(num > currentCard) {  // below current card
+					animate(cc, -viewportWidth, easeAmt);
+					
+					cards.removeClass('current').removeClass('top');
+					nc.addClass('current');
+					cc.addClass('top');
+				} else {  // above current card
+					animate(nc, -viewportWidth, 'none');
+					animate(nc, 0, easeAmt);
+					
+					cards.removeClass('current');
+					nc.removeClass('top').addClass('current');
 				}
-
-				// Trigger event
-				parent.trigger('update');
+				// Update current slide
+				currentCard = num;
 			}
+
+			// Control Buttons
+			updateControls();
+
+			console.log(num);
 		}
 	},
 
 	updateControls = function() {
 		// Enable control buttons
-		if(currentSlide>0 && currentSlide<numSlides-1) {
+		if(currentCard>0 && currentCard<numSlides-1) {
 			$('.control', el).show();
-		} else if (currentSlide<=0) {
+		} else if (currentCard<=0) {
 			$('.control.prev').hide();
-			if(!defaults.preventAdvance || currentSlide==0) {
+			if(!defaults.preventAdvance || currentCard==0) {
 				$('.control.next').show();
 			}
-		} else if (currentSlide>=numSlides-1) {
+		} else if (currentCard>=numSlides-1) {
 			$('.control.next').hide();
 			$('.control.prev').show();
 		}
-	},
-
-	disableSliding = function() {
-		// Hide Controls
-		$('.control', el).hide();
-		// Add disabled flag
-		parent.addClass('disabled');
-	},
-
-	enableSliding = function() {
-		// Enable control buttons
-		updateControls();
-		// Remove disabled flag
-		parent.removeClass('disabled');
 	};
 
 	// Initialize the object
@@ -240,22 +236,18 @@ var Deck = function(selector, options) {
 
 		jumpToSlide : jumpToSlide,
 
-		disableSliding : disableSliding,
-
-		enableSliding : enableSliding,
-
 		current : function() {
-			return currentSlide+1;
+			return currentCard+1;
 		},
 
 		total : numSlides,
 
 		next : function() {
-			jumpToSlide(currentSlide+1);
+			jumpToSlide(currentCard+1);
 		},
 
 		prev : function() {
-			jumpToSlide(currentSlide-1);  
+			jumpToSlide(currentCard-1);  
 		}
 	};
 
